66b2899807e5129ee660423023e1ce3837b867b8
vitorluzsantos <vitor.lsantos@sptech.school>
2026-06-28 10:14:48 -0300

    feat: exercicio 1006 e 1007

---
+++ 1006.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float A, B, C;
+
+	scanf("%f", &A);
+	scanf("%f", &B);
+	scanf("%f", &C);
+
+	float MEDIA = ((A * 2) + (B * 3) + (C * 5))/10;
+	printf("MEDIA = %.1f\n", MEDIA);
+
+	return 0;
+}
